71a3163910d6d50bca8f60093498cdbef02c8cbd
BBpeter1 <79256968+BBpeter1@users.noreply.github.com>
2021-09-29 15:31:11 +0200

    Add files via upload

---
+++ ASNQPH_0915/BartokBalogPeterFileOlvas.java
@@ -0,0 +1,25 @@
+package bbpgyak3;
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+
+public class BartokBalogPeterFileOlvas {
+
+	public static void main(String[] args) {
+		
+		try {
+		      File obj = new File("fajl.txt");
+		      Scanner Reader = new Scanner(obj);
+		      while (Reader.hasNextLine()) {
+		        String data = Reader.nextLine();
+		        System.out.println(data);
+		      }
+		      Reader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("Hiba történt az olvasás során!");
+		      e.printStackTrace();
+		    }
+		  }
+
+		
+}
